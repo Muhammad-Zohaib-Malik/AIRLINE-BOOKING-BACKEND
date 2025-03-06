137e67d670e097404b1a512dabe91bc05f930f5a
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-06 17:28:21 +0500

    Initialize server setup with Express and database connection

---
+++ FLIGHTS/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import apiRoutes from './routes/index.js'
+import logger from './utils/logger.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB()
+
+app.use(express.json());
+app.use('/api', apiRoutes)
+
+app.listen(port, () => {
+  logger.info(`Server is running on port ${port}`)
+});
+
