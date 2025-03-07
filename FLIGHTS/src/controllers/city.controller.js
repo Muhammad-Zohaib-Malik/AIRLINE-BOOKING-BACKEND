161005ef3ed77d00aa07ac3e36ade2b0073b08da
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-07 22:08:02 +0500

    Add error handling for city creation and deletion, and ensure related airports are removed

--- FLIGHTS/src/controllers/city.controller.js
+++ FLIGHTS/src/controllers/city.controller.js
@@ -3,11 +3,21 @@ import { validateCity } from "../utils/validation.js";
 import { City } from '../models/city.model.js'
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import { StatusCodes } from 'http-status-codes'
+import mongoose from 'mongoose'
+import { Airport } from "../models/airport.model.js";
 
 export const createCity = asyncHandler(async (req, res) => {
-  const validatedData = validateCity(req.body);
-  const city = await City.create(validatedData);
-  return res.status(StatusCodes.CREATED).json(new ApiResponse(StatusCodes.CREATED, city, "City created successfully"))
+  try {
+    const validatedData = validateCity(req.body);
+    const city = await City.create(validatedData);
+    return res.status(StatusCodes.CREATED).json(new ApiResponse(StatusCodes.CREATED, city, "City created successfully"))
+
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new ApiError(StatusCodes.CONFLICT, "City with this name already exists");
+    }
+  }
 
 })
 
@@ -33,11 +43,14 @@ export const deleteCityById = asyncHandler(async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid city ID format");
   }
-  const city = await City.findById(id);
+  const city = await City.findOneAndDelete(id);
   if (!city) {
     throw new ApiError(StatusCodes.NOT_FOUND, "city not found");
   }
-  return res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, city, "city deleted successfully"))
+
+  await Airport.deleteMany({ city: id })
+
+  return res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, city, "city  and related airports deleted successfully"))
 })
 
 export const updateCityById = asyncHandler(async (req, res) => {
@@ -46,9 +59,9 @@ export const updateCityById = asyncHandler(async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid city ID format");
   }
-  const city = await Airplane.findByIdAndUpdate(id, validatedData, { new: true });
+  const city = await City.findByIdAndUpdate(id, validatedData, { new: true });
   if (!city) {
     throw new ApiError(StatusCodes.NOT_FOUND, "city not found");
   }
-  return res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, airplane, "city updated successfully"))
+  return res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, city, "city updated successfully"))
 })
