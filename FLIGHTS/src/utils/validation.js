f9881a215ded3eb855a81038d8abb286640b73da
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-07 15:37:37 +0500

    Add validation schemas and functions for airplane and airport models

--- FLIGHTS/src/utils/validation.js
+++ FLIGHTS/src/utils/validation.js
@@ -1,5 +1,6 @@
 import { z } from "zod"
 
+// airplane validation
 export const airplaneSchema = z.object({
   airLine: z.string().trim().min(1, "Airline is required and must be a string"),
   modelNumber: z.string().trim().min(1, "Model number is required and must be a string"),
@@ -7,22 +8,26 @@ export const airplaneSchema = z.object({
   status: z.string().optional(),
 })
 
-export const validateAirplane = (data) => {
-  const parsedBody = airplaneSchema.safeParse(data);
-  if (!parsedBody.success) {
-    const errorMessage = parsedBody.error.errors.map(err => err.message).join(", ");
-    throw new Error(errorMessage);
-  }
-  return parsedBody.data;
-};
-
+// city validation
 
 export const citySchema = z.object({
   name: z.string().trim().min(1, "City name is required and must be a string"),
 })
 
-export const validateCity = (data) => {
-  const parsedBody = airplaneSchema.safeParse(data);
+// airport validation
+
+export const airportSchema = z.object({
+  name: z.string().trim().min(1, "Name is required and must be a string"),
+  code: z.string().trim().min(1, "Code is required and must be a string"),
+  city: z.string().length(24, "City ID must be a valid ObjectId"),
+  address: z.string().trim().min(5, "Address is required")
+})
+
+
+
+
+export const validateData = (schema, data) => {
+  const parsedBody = schema.safeParse(data);
   if (!parsedBody.success) {
     const errorMessage = parsedBody.error.errors.map(err => err.message).join(", ");
     throw new Error(errorMessage);
@@ -30,3 +35,6 @@ export const validateCity = (data) => {
   return parsedBody.data;
 };
 
+export const validateAirplane = (data) => validateData(airplaneSchema, data);
+export const validateCity = (data) => validateData(citySchema, data);
+export const validateAirport = (data) => validateData(airportSchema, data);
