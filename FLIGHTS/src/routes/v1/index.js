1bb2e791bcb0c923d0fdab37d026526044daeb59
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-30 16:28:38 +0500

    Refactor flight routes to include filtering functionality and improve code formatting for consistency

--- FLIGHTS/src/routes/v1/index.js
+++ FLIGHTS/src/routes/v1/index.js
@@ -1,48 +1,63 @@
-import express from 'express'
-import { createAirplane, deleteAirplaneById, getAirplaneById, getAllAirplanes, updateAirplaneById } from '../../controllers/airplane.controller.js'
-import { createCity, deleteCityById, getAllCities, getCityById, updateCityById } from '../../controllers/city.controller.js'
-import { createAirport, deleteAirportById, getAirportById, getAllAirports, updateAirportById } from '../../controllers/airport.controller.js'
-import { createFlight, deleteFlightById, getAllFlights, getFlightById, updateFlightById } from '../../controllers/flight.controller.js'
+import express from "express";
+import {
+  createAirplane,
+  deleteAirplaneById,
+  getAirplaneById,
+  getAllAirplanes,
+  updateAirplaneById,
+} from "../../controllers/airplane.controller.js";
+import {
+  createCity,
+  deleteCityById,
+  getAllCities,
+  getCityById,
+  updateCityById,
+} from "../../controllers/city.controller.js";
+import {
+  createAirport,
+  deleteAirportById,
+  getAirportById,
+  getAllAirports,
+  updateAirportById,
+} from "../../controllers/airport.controller.js";
+import {
+  createFlight,
+  deleteFlightById,
+  getAllFlights,
+  getAllFlightsWithFilters,
+  getFlightById,
+  updateFlightById,
+} from "../../controllers/flight.controller.js";
 
-
-const router = express.Router()
-// airplane 
-router.post('/airplane', createAirplane)
-router.get('/airplane', getAllAirplanes)
-router.get('/airplane/:id', getAirplaneById)
-router.delete('/airplane/:id', deleteAirplaneById)
-router.patch('/airplane/:id', updateAirplaneById)
-
+const router = express.Router();
+// airplane
+router.post("/airplane", createAirplane);
+router.get("/airplane", getAllAirplanes);
+router.get("/airplane/:id", getAirplaneById);
+router.delete("/airplane/:id", deleteAirplaneById);
+router.patch("/airplane/:id", updateAirplaneById);
 
 // city
-router.post('/city', createCity)
-router.get('/city', getAllCities)
-router.get('/city/:id', getCityById)
-router.delete('/city/:id', deleteCityById)
-router.patch('/city/:id', updateCityById)
-
+router.post("/city", createCity);
+router.get("/city", getAllCities);
+router.get("/city/:id", getCityById);
+router.delete("/city/:id", deleteCityById);
+router.patch("/city/:id", updateCityById);
 
 // airport
-router.post('/airport', createAirport)
-router.get('/airport', getAllAirports)
-router.get('/airport/:id', getAirportById)
-router.delete('/airport/:id', deleteAirportById)
-router.patch('/airport/:id', updateAirportById)
+router.post("/airport", createAirport);
+router.get("/airport", getAllAirports);
+router.get("/airport/:id", getAirportById);
+router.delete("/airport/:id", deleteAirportById);
+router.patch("/airport/:id", updateAirportById);
 
 //flight
-router.post('/flight', createFlight)
-router.get('/flight', getAllFlights)
-router.get('/flight/:id', getFlightById)
-router.delete('/flight/:id', deleteFlightById)
-router.patch('/flight/:id', updateFlightById)
+router.post("/flight", createFlight);
+// router.get("/flight", getAllFlights);
+router.get("/flight", getAllFlightsWithFilters);
 
+router.get("/flight/:id", getFlightById);
+router.delete("/flight/:id", deleteFlightById);
+router.patch("/flight/:id", updateFlightById);
 
-
-
-
-
-
-
-
-
-export default router
+export default router;
