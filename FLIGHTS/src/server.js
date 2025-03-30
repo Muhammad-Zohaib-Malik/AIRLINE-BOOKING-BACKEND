8ddb57c479757e78a0480a5a73731a74ea1bdd44
zohaib <muhammadzohaibmalik10@gmail.com>
2025-03-30 16:28:45 +0500

    feat: enable URL-encoded data parsing in server configuration

--- FLIGHTS/src/server.js
+++ FLIGHTS/src/server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 connectDB()
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes)
 
 app.listen(port, () => {
