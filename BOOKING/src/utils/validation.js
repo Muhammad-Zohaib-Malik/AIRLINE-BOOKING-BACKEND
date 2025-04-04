70d77ac2c759df7322a2aaa95b8904e0feef9e97
zohaib <muhammadzohaibmalik10@gmail.com>
2025-04-04 19:08:48 +0500

    added validation

--- BOOKING/src/utils/validation.js
+++ BOOKING/src/utils/validation.js
@@ -1,4 +1,20 @@
 import { z } from "zod";
+import mongoose from "mongoose";
+
+
+export const bookingSchema = z.object({
+  flightId: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), {
+    message: "Invalid Flight ID format",
+  }),
+  // userId: z.string().refine((id) => mongoose.Types.ObjectId.isValid(id), {
+  //   message: "Invalid User ID format",
+  // }),
+  // status: z.enum(["Confirmed", "Pending", "Initiated", "Cancelled"]).default("Initiated"),
+  noOfSeats: z.number().int().positive().min(1).max(10), 
+  seatClass: z.enum(["economy", "business"]),
+  // totalCost: z.number().positive(),
+});
+
 
 
 export const validateData = (schema, data) => {
@@ -12,3 +28,5 @@ export const validateData = (schema, data) => {
   return parsedBody.data;
 };
 
+
+export const validateBooking = (data) => validateData(bookingSchema, data);
