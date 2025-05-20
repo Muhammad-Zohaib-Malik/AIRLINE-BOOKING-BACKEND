50398a71138128c9a661a3ebeee3a5d6e6af236d
zohaib <muhammadzohaibmalik10@gmail.com>
2025-05-20 06:16:28 +0500

    refactor: standardize code formatting in server.js

--- NOTIFICATION/src/server.js
+++ NOTIFICATION/src/server.js
@@ -1,21 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import apiRoutes from './routes/index.js'
-import logger from './utils/logger.js';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import apiRoutes from "./routes/index.js";
+import logger from "./utils/logger.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3002;
 
-connectDB()
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
-app.listen(port, () => {
-  logger.info(`Server is running on port ${port}`)
+app.listen(port, async () => {
+  logger.info(`Server is running on port ${port}`);
 });
-
